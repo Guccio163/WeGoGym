2b2756a5f3cbac8a4f283619570f0c6d28a7ea26
Kajetan <kajetanozog@gmail.com>
2024-03-16 13:13:53 +0100

    open_now

--- back/FASTapi.py
+++ back/FASTapi.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 import json
+import datetime
 
 app = FastAPI()
 
@@ -10,16 +11,45 @@ def read_all_data():
     return data
 
 
+def is_time_between(time_str, start_time_str, end_time_str):
+    time = datetime.datetime.strptime(time_str, '%H:%M').time()
+    start_time = datetime.datetime.strptime(start_time_str, '%H:%M').time()
+    end_time = datetime.datetime.strptime(end_time_str, '%H:%M').time()
+
+    # Check if the given time is between start and end times
+    if start_time <= time <= end_time:
+        return True
+    else:
+        return False
+
+
 @app.get("/all_data")
 async def read_root():
     data = read_all_data()
     return data
 
 
-@app.get("/by_price")
+@app.get("/by_price/month")
 async def read_by_price():
     data = read_all_data()
     return data
-    
 
+
+@app.get("/open_now")
+async def read_by_price():
+    data = read_all_data()
+    response = {}
+    for d in data["gyms"]:
+        current_date = datetime.datetime.now()
+        day_of_week = current_date.strftime("%A").lower()
+        open_hour = d["opening hours"][day_of_week]
+        start_time_str, end_time_str = open_hour.split(" - ")
+        current_time = datetime.datetime.now().time()
+        hour = current_time.hour
+        minute = current_time.minute
+        time_str = str(hour) + ":" + str(minute)
+        print("!")
+        if is_time_between(time_str, start_time_str, end_time_str):
+            response.update(d)
+    return response
 
--- back/request.py
+++ back/request.py
@@ -1,4 +1,4 @@
 import requests
 
-response = requests.get("http://127.0.0.1:8000/all_data")
+response = requests.get("http://127.0.0.1:8000/open_now")
 print(response.json())
