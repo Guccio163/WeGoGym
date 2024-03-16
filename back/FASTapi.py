04fad1207f79681fce5f82a135a82498fc53b6ed
Oskar <oskarkulinski3@gmail.com>
2024-03-16 12:18:21 +0100

    added honored cards

--- back/FASTapi.py
+++ back/FASTapi.py
@@ -16,3 +16,15 @@ async def get_data():
     return gyms_data
 
 
+@app.get("/data/gyms")
+async def get_data():
+    # Read data from JSON file
+    with open("data.json", "r") as file:
+        data = json.load(file)
+
+    # Extract gyms field
+    gyms_data = data.get("gyms")
+
+    return gyms_data
+
+
